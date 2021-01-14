aeace654b914482a67790fd3e80c32c11746f724
Joe Kovalik-Haas <jkovalikhaas1@gmail.com>
2021-01-14 14:31:30 -0500

    added admplify yml file

--- src/globals/fetchAPI.js
+++ src/globals/fetchAPI.js
@@ -2,7 +2,7 @@ import { DataStore } from '@aws-amplify/datastore';
 import { Words } from '../models';
 
 import Amplify from "aws-amplify";
-import awsExports from "../aws-exports";
+import awsExports from "../aws-exports.js";
 Amplify.configure(awsExports);
 
 export const fetchWords = async () => {
